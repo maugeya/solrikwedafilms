08815f04a0d9178497040ea821c373dd1f7414d1
Angela Maugey <a.maugey@gmail.com>
2017-08-23 22:23:23 +0100

    scroll function added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 $(() => {
   console.log('JS loaded');
 
-  
+//section scroll function
+
+  $('a').on('click', scrollToSection);
+
+  function scrollToSection() {
+    const section = $(this).attr('href');
+    $('body', 'html').animate({
+      scrollTop: $(section).offset().top
+    }, 1300);
+  }
+
+
+
 });
